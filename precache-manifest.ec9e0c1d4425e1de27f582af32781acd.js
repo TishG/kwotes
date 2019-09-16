6175a7849c5eda5e9930594c17d6e5d7d02fed60
Tish <latisha.griffiths@programmer.net>
2019-09-16 07:49:45 -0500

    Updates

---
+++ precache-manifest.ec9e0c1d4425e1de27f582af32781acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3cac091ba8072b9c4ff79cb87c1ceb0",
+    "url": "/kwotes/index.html"
+  },
+  {
+    "revision": "aa8cfb465c6b36778004",
+    "url": "/kwotes/static/css/main.cb6fe338.chunk.css"
+  },
+  {
+    "revision": "6c74877f4d3dffa2fbe5",
+    "url": "/kwotes/static/js/2.d820c5dc.chunk.js"
+  },
+  {
+    "revision": "aa8cfb465c6b36778004",
+    "url": "/kwotes/static/js/main.644f7dab.chunk.js"
+  },
+  {
+    "revision": "dbdb4ef1a02e9727703b",
+    "url": "/kwotes/static/js/runtime~main.14aaedb8.js"
+  }
+]);
