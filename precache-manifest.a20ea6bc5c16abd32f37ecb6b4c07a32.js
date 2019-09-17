4e38b8f7da6d7bced0bdbe9b1b2a58570ad881f5
Tish <latisha.griffiths@programmer.net>
2019-09-17 11:32:16 -0500

    Updates

--- precache-manifest.a20ea6bc5c16abd32f37ecb6b4c07a32.js
+++ precache-manifest.a20ea6bc5c16abd32f37ecb6b4c07a32.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "84628f51e12aa7a6954b5f3c195d9b76",
+    "revision": "cea749c7470317da78ee4df7a7dc9362",
     "url": "/kwotes/index.html"
   },
   {
-    "revision": "f929f0ce034853ec14b1",
+    "revision": "e462e3c31a514b6b6db3",
     "url": "/kwotes/static/css/main.428d68b5.chunk.css"
   },
   {
-    "revision": "de8d3fa179a67506dc0e",
-    "url": "/kwotes/static/js/2.fdada4e7.chunk.js"
+    "revision": "6c74877f4d3dffa2fbe5",
+    "url": "/kwotes/static/js/2.d820c5dc.chunk.js"
   },
   {
-    "revision": "f929f0ce034853ec14b1",
-    "url": "/kwotes/static/js/main.f54b6f2b.chunk.js"
+    "revision": "e462e3c31a514b6b6db3",
+    "url": "/kwotes/static/js/main.d8b8fdd1.chunk.js"
   },
   {
     "revision": "dbdb4ef1a02e9727703b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kwotes/precache-manifest.61e30be8ab0fadbaf29842c75ad4cde9.js"
+  "/kwotes/precache-manifest.a20ea6bc5c16abd32f37ecb6b4c07a32.js"
 );
 
 self.addEventListener('message', (event) => {
